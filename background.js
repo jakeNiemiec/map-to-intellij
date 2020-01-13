de16af4acaf32aea1fb0ba5654a5bc426948ffde
Henning Kvinnesland <henning.kvinnesland@statnett.no>
2020-01-13 16:15:43 +0100

    Add support for custom-regex.

--- background.js
+++ background.js
@@ -1,27 +1,38 @@
-const regex = /^file:\/\/(.+?\.(jsx?|s?css)(?:.+)?)$/;
-
 function defaultCB(url, line) {
   chrome.devtools.panels.openResource(url, line);
 }
-function lawdFile(e, line) {
-  var url = e.url;
-  console.log(url,regex,regex.test(url));
-  if (!regex.test(url)) {
-    console.debug('opening in sources', url);
-    defaultCB(url, line);
-  }
-  else {
-    url = url.replace(regex, 'http://127.0.0.1:63342/api/file/$1');
-    console.debug('calling',url);
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url + (':' + line || ''), true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == XMLHttpRequest.DONE && xhr.status !== 200) {
-        // Request finished. Do processing here.
+
+function registerLoader() {
+  chrome.storage.sync.get({ pattern: '/^file:\\/\\/(.+?\\.(jsx?|s?css)(?:.+)?)$/' }, function(items) {
+    var pattern = items.pattern;
+    var regex = new RegExp(pattern);
+
+    function lawdFile(e, line) {
+      var url = e.url;
+      console.log(url,regex,regex.test(url));
+
+      if (!regex.test(url)) {
+        console.log('opening in sources', url);
         defaultCB(url, line);
       }
-    };
-    xhr.send();
-  }
+      else {
+        url = url.replace(regex, 'http://127.0.0.1:63342/api/file/$1');
+        console.log('calling',url);
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url + (':' + line || ''), true);
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState == XMLHttpRequest.DONE && xhr.status !== 200) {
+            // Request finished. Do processing here.
+            defaultCB(url, line);
+          }
+        };
+        xhr.send();
+      }
+    }
+
+    chrome.devtools.panels.setOpenResourceHandler(lawdFile);
+
+  });
 }
-chrome.devtools.panels.setOpenResourceHandler(lawdFile);
+
+registerLoader();
